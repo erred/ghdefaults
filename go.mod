83cf1ec45b3975ab1c7e9b0ee1f4cd0dda5f3b37
Sean Liao <seankhliao@gmail.com>
2020-09-26 17:45:02 +0200

    remove stream

--- go.mod
+++ go.mod
@@ -6,8 +6,6 @@ require (
 	github.com/bradleyfalzon/ghinstallation v1.1.1
 	github.com/google/go-github/v32 v32.1.0
 	github.com/rs/zerolog v1.18.0
-	go.opentelemetry.io/contrib/instrumentation/net/http/httptrace v0.11.0 // indirect
-	go.seankhliao.com/stream v0.1.0
 	go.seankhliao.com/usvc v0.7.7
-	google.golang.org/grpc v1.32.0
+	golang.org/x/sys v0.0.0-20200826173525-f9321e4c35a6 // indirect
 )
--- main.go
+++ main.go
@@ -4,17 +4,13 @@ import (
 	"context"
 	"encoding/base64"
 	"flag"
-	"fmt"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/bradleyfalzon/ghinstallation"
 	"github.com/google/go-github/v32/github"
 	"github.com/rs/zerolog"
-	"go.seankhliao.com/stream"
 	"go.seankhliao.com/usvc"
-	"google.golang.org/grpc"
 )
 
 var (
@@ -34,16 +30,11 @@ type Server struct {
 	pkey          []byte
 
 	log zerolog.Logger
-
-	streamAddr string
-	client     stream.StreamClient
-	cc         *grpc.ClientConn
 }
 
 func (s *Server) Flag(fs *flag.FlagSet) {
 	fs.StringVar(&s.WebHookSecret, "webhook-secret", os.Getenv("WEBHOOK_SECRET"), "webhook validation secret")
 	fs.StringVar(&s.privBase64, "priv", os.Getenv("PRIVATE_KEY"), "base64 encoded private key")
-	fs.StringVar(&s.streamAddr, "stream.addr", "stream:80", "url to connect to stream")
 }
 
 func (s *Server) Register(c *usvc.Components) error {
@@ -56,22 +47,14 @@ func (s *Server) Register(c *usvc.Components) error {
 	}
 
 	c.HTTP.Handle("/", s)
-
-	s.cc, err = grpc.Dial(s.streamAddr, grpc.WithInsecure())
-	if err != nil {
-		return fmt.Errorf("connect to stream: %w", err)
-	}
-	s.client = stream.NewStreamClient(s.cc)
 	return nil
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
-	return s.cc.Close()
+	return nil
 }
 
 func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
 	payload, err := github.ValidatePayload(r, []byte(s.WebHookSecret))
 	if err != nil {
 		s.log.Error().Err(err).Msg("webhook validation failed")
@@ -114,16 +97,5 @@ func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		s.log.Info().Str("owner", o).Str("repo", re).Msg("defaults set")
-
-		repoRequest := &stream.RepoRequest{
-			Timestamp: time.Now().Format(time.RFC3339),
-			Owner:     o,
-			Repo:      re,
-		}
-
-		_, err = s.client.LogRepo(ctx, repoRequest)
-		if err != nil {
-			s.log.Error().Err(err).Msg("write to stream")
-		}
 	}
 }
